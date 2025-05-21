678b954c91900f805f9e0f53ce9232c9cf90ae78
namdh03 <duonghoangnam503@gmail.com>
2025-05-21 13:38:24 +0700

    chore: handle cors

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,11 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+  });
 
   await app.listen(port ?? 3000);
 }
